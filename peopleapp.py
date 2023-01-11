3041ec42b5fe4410946eb63623205fb2cc694e47
mrzkx <45421136+mrzkx@users.noreply.github.com>
2023-01-11 18:44:47 +0800

    Add files via upload

---
+++ peopleapp.py
@@ -0,0 +1,14 @@
+params_dict = {
+"class_id":"37",
+"page": "1",
+"version":"7.2.5.1",
+"timestamp": "1673430148"
+}
+key_list = []
+for key in params_dict.keys():
+    key_list.append(key)
+key_list.sort()
+str4 = ""
+for k in key_list:
+    str4 = str4 + params_dict.get(k) + "|"
+print(str4.rstrip('|')+"rmrbsecurity$#%sutf3d247a75d9c7c4")
